9d00ea8d6ab3a780db1884961b5e0d1b5d47e225
roavinash <roaavinash@gmail.com>
2022-11-04 15:30:47 +0530

    new

---
+++ 1DSA(04-11-2022)/StringMethodDemo.java
@@ -0,0 +1,19 @@
+package dsa10August2022;
+
+import java.util.Scanner;
+
+public class StringMethodDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner avi=new Scanner(System.in);
+        int x1=avi.nextInt();
+	        int y1=avi.nextInt();
+	        System.out.println();
+	        int x2=avi.nextInt();
+	        int y2=avi.nextInt();
+	        double d=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	        System.out.println(d);
+	}
+
+}
