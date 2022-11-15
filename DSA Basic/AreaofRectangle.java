98becd22a5d634c5e37ba6c2edcb98f22ccea6af
roavinash <roaavinash@gmail.com>
2022-11-15 09:47:16 +0530

    Dsa Basic

---
+++ DSA Basic/AreaofRectangle.java
@@ -0,0 +1,14 @@
+package _01DSA04_11_2022;
+
+public class AreaofRectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int length=78;
+	int breath=75;
+	int area=length*breath;
+	System.out.println(area);
+	}
+
+}
